e9278c77a71de232f012bd9de781dcd7d50bc03d
utkarshkumar29 <ukyp2014@gmail.com>
2026-03-04 09:01:24 +0530

    project module addition #3

---
+++ app/(dashboard)/projects/page.tsx
@@ -0,0 +1,9 @@
+const Projects=()=>{
+    return(
+        <div className=" h-screen flex items-center justify-center ">
+            <h1 className=" text-2xl text-gray-400 ">Projects Page</h1>
+        </div>
+    )
+}
+
+export default Projects
